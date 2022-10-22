dc42e145c96b3ae6862b79dfd6b1ed364703fcae
Juan David Lopera <juanl@backbase.com>
2022-10-22 18:37:29 -0500

    Testing Design System
    Testing apply style function DONE
    Testing Text Color palette DONE
    Testing TextConfiguration DONE

--- RecipeBook/DesignSystem/Colors/TextColor/TextColor.swift
+++ RecipeBook/DesignSystem/Colors/TextColor/TextColor.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-fileprivate let chalkBlack: UIColor = UIColor(red: 38/255, green: 36/255, blue: 27/255, alpha: 1.0)
-fileprivate let transparentBlack: UIColor = UIColor(red: 39/255, green: 39/255, blue: 39/255, alpha: 1.0)
+let chalkBlack: UIColor = UIColor(red: 38/255, green: 36/255, blue: 27/255, alpha: 1.0)
+let transparentBlack: UIColor = UIColor(red: 39/255, green: 39/255, blue: 39/255, alpha: 1.0)
 
 public enum TextColor {
     case primary
