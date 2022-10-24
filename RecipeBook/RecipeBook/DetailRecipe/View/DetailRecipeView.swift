d7afe98d45476944898efb4c852ca6e51b9194fc
Juan David Lopera <juanl@backbase.com>
2022-10-24 11:53:29 -0500

    Detail Recipe View
    Detail Recipe View Initial

---
+++ RecipeBook/RecipeBook/DetailRecipe/View/DetailRecipeView.swift
@@ -0,0 +1,13 @@
+//
+//  DetailRecipeView.swift
+//  RecipeBook
+//
+//  Created by Juan david Lopera lopez on 24/10/22.
+//
+
+import Foundation
+import UIKit
+
+final class DetailRecipeView: UIView {
+    
+}
